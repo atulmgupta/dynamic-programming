2e0ba1aea0a4ca815ff55c66b5740cba2332e673
atul <iamatulmgupta@gmail.com>
2020-08-15 18:34:16 -0700

    Added Initial Commit

---
+++ src/main/java/com/atul/dp/DynamicProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.atul.dp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atul/dp/DynamicProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atul.dp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
